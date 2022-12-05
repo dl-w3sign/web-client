6fbbad097e52ea3fde34afe631b5927abc6844b2
Ruslan Kasheparov <ruslankasheparov@gmail.com>
2022-12-05 14:21:22 +0000

    Develop

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { config } from '@/config'
 
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: config.API_URL,
   headers: {
     'Content-type': 'application/json',
   },
