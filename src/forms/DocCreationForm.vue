c599e90ba0a90e7824514dff7ebab27db53b60ad
Yehor Podporinov <50983498+yehor-podporinov@users.noreply.github.com>
2023-02-24 13:22:23 +0200

    fix fee value in createStamp invoke

--- src/forms/DocCreationForm.vue
+++ src/forms/DocCreationForm.vue
@@ -267,7 +267,7 @@ const submit = async () => {
       form.isIndicatingAddresses ? form.indicatedAddresses.reverse() : [],
       ZKPPointsStruct,
       {
-        value: 0,
+        value: fee.value as BigNumberish,
       },
     )
 
